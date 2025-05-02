a194180216039a8620da25a76294526468f09012
chan you <cksdb95@gmail.com>
2025-05-02 17:55:14 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import pytest
 
-def add(a,b):
+def add(a, b):
     return a + b
 
 def test_add_positive_numbers():
-    assert add(2,3) == 5, "2 + 3은 5여야 합니다"# (이곳에 답을 작성하세요)
+    # 2 + 3은 5여야 한다는 것을 테스트합니다
+    assert add(2, 3) == 5, "2 + 3은 5여야 합니다."
